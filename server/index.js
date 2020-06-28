4f7076ddb67475d3683ca556436aa8a3672cf03e
suraaloqaily <aloqailysura@gmail.com>
2020-06-28 16:46:06 +0300

    DB Ready and start working in express

--- server/index.js
+++ server/index.js
@@ -1,24 +1,34 @@
+var path=require("path")
+//install express 
 var express = require('express');
-var bodyParser = require('body-parser');
-
-var items = require('../database-mongo');
-
+//app is instance from our express
 var app = express();
+// npm install body-parser
+// middleware
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({	extended:true}));
+app.use(bodyParser.json())
+var items = require('../database-mongo');
 
 // UNCOMMENT FOR REACT
 app.use(express.static(__dirname + '/../react-client/dist'));
+app.post('/',function (req, res) {
+	console.log(req.body)
+items.insertIntoAutho(req.body)
+})
+app.get('/', function (req, res) {
+  // items.selectAllfromTeacher(function(err, data) {
+  //   if(err) {
+  //     res.sendStatus(500);
+  //   } else {
+  //     console.log(data)
+  //   }
+  // });
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
+ // console.log(items.selectAllfromTeacher()) 
 });
 
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
+app.listen(3110, function() {
+  console.log('listening on port 3100!');
 });
 
