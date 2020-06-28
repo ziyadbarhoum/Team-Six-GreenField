aae901a2c953b52ae260766b90cc5ae01cae2b34
oriebalzyuot96 <=>
2020-06-28 10:01:19 +0300

    connection is done

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,3 +1,5 @@
+
+//connection is done 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
@@ -11,21 +13,17 @@ db.once('open', function() {
   console.log('mongoose connected successfully');
 });
 
-var itemSchema = mongoose.Schema({
-  quantity: Number,
-  description: String
-});
 
-var Item = mongoose.model('Item', itemSchema);
 
-var selectAll = function(callback) {
-  Item.find({}, function(err, items) {
-    if(err) {
-      callback(err, null);
-    } else {
-      callback(null, items);
-    }
-  });
-};
 
-module.exports.selectAll = selectAll;
+// var selectAll = function(callback) {
+//   Item.find({}, function(err, items) {
+//     if(err) {
+//       callback(err, null);
+//     } else {
+//       callback(null, items);
+//     }
+//   });
+// };
+
+//module.exports.selectAll = selectAll;
