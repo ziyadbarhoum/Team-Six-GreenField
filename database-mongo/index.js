cf12f600fa1e147c21d4b11f23744ef93740ead0
suraaloqaily <aloqailysura@gmail.com>
2020-06-28 11:17:05 +0300

    building controllers

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -11,7 +11,7 @@ db.once('open', function() {
   console.log('mongoose connected successfully');
 });
 
-const teacherSchema = new Schema({
+const authoSchema = new Schema({
 
   email: {type: String,
       required: true,
@@ -21,19 +21,62 @@ const teacherSchema = new Schema({
   password:{type: String,
       required: true,
        minlength: 8
-  });
+  }
+});
 
+var obj1={
+  email:"defult@sura.com",
+  password:"defult"
+}
+
+
+var obj2={
+  name:"sura",
+  phone:"0000",
+  place:"irbed",
+  subject:"arabic",
+  description:"Anything",
+  price:"12$",
+  email:"sura@sura.com"
+}
 
 const Teacher = mongoose.model('Teacher', teacherSchema);
-
-var selectAll = function(callback) {
-  Item.find({}, function(err, items) {
+const Autho = mongoose.model('Autho', authoSchema);
+//select all from teachers
+var selectAllfromTeacher = function(callback) {
+  Teacher.find({}, function(err, teachers) {
     if(err) {
       callback(err, null);
     } else {
-      callback(null, items);
+      callback(null, teachers);
+    }
+  });
+};
+// insert into autho schema 
+var insertIntoAutho = function(callback) {
+  Autho.insertOne(obj1, function(err, data) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, data);
+    }
+  });
+};
+// insert into Teacher schema 
+var insertIntoTeacher = function(callback) {
+  Teacher.insertOne(obj2, function(err, data) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, data);
     }
   });
 };
 
-module.exports.selectAll = selectAll;
+
+
+
+
+module.exports.insertIntoAutho = insertIntoAutho;
+module.exports.insertIntoTeacher = insertIntoTeacher;
+module.exports.selectAllfromTeacher = selectAllfromTeacher;
