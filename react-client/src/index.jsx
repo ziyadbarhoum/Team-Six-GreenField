298730edef03d3605ba5ec42ca225ea20c239497
Afnan shawawreh <64199018+AfnanShawawreh@users.noreply.github.com>
2020-06-30 13:29:48 +0300

    play with react and create new files 'main page ,search , app '

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,36 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import List from './components/List.jsx';
+import App from './app.jsx';
+import searchPage from './components/search.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      items: []
-    }
-  }
-
-  componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
-  }
-
-  render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>)
-  }
-}
 
 ReactDOM.render(<App />, document.getElementById('app'));
