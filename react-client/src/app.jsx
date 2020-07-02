281426930d46472cbdccc3a54dc5fa4782593322
Afnan shawawreh <64199018+AfnanShawawreh@users.noreply.github.com>
2020-07-02 13:09:06 +0300

    add new components and refactor

--- react-client/src/app.jsx
+++ react-client/src/app.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import SearchPage from './components/search.jsx';
 
-import SignInSignUpForm from './components/authentication.jsx';
+
 import {
     BrowserRouter as Router,
     Switch,
@@ -10,34 +9,84 @@ import {
     useRouteMatch,
     useParams
   } from "react-router-dom";
+  import TeacherForm from './components/formData.jsx';
+
+  import SignInSignUpForm from './components/authentication.jsx';
+  import LoginForm from './components/login.jsx';
+  import SearchPage from './components/search.jsx';
+  import SearchTe from './components/searchteach.jsx';
+  import SignUpForm from './components/signup.jsx';
+  import First from './components/techeardata.jsx';
+
+  ;
+  export default class App extends React.Component {
+    render () {
+      return (
+      <Router basename="">
+        <div className="App">
+          <div className="App__Aside"></div>
+          <div className="App__Form">
+            <div className="PageSwitcher">
+                              </div>
+              <div className="FormTitle">
+              </div>
+              <div>
+//         <h1>BOOK TEACHER </h1>
+//       </div>
+                      <button><Link to="/teacher">TEACHER</Link></button>
+          <button><Link to="/STUDENT">STUDENT</Link></button>
+              <Route  path="/student" component={SearchPage}>
+              </Route>
+              <Route path="/teacher" component={SignInSignUpForm}>
+              </Route>
 
 
+              <Route path="/teacherForm" component={SignUpForm}>
+              </Route>
+              <Route path="/view" component={First}>
+              </Route>
+              <Route path="/SignUpform">
+            <TeacherForm/>
+          </Route>
+          <Route path="/see">
+            <SearchTe/>
+          </Route>
+          <Route path="/loginform">
+            <LoginForm/>
+          </Route>
+          <Route path="/SearchTe">
+            < SearchTe/>
+          </Route>
+          </div>
+        </div>
+      </Router>
 
-export default class App extends React.Component {
-  render () {
-    return (
-    <div>
+         );
+    }
+  }
+
+{/* <div>
     <Header/>
     <Content/>
     </div>);
   }
-}
-class Header extends React.Component{
-  render(){
-    return(
-      <div>
-        <h1>BOOK TEACHER </h1>
-      </div>
-    )
-  }
-}
-class Content extends React.Component{
-  render(){
-    return(
+} */}
+// class Header extends React.Component{
+//   render(){
+//     return(
+//       <div>
+//         <h1>BOOK TEACHER </h1>
+//       </div>
+//     )
+//   }
+// }
+// class Content extends React.Component{
+//   render(){
+//     return(
 
-<Router>
-      <div>
-
+// <Router>
+//       <div>
+{/*
 
         <button><Link to="/TEACHER">TEACHER</Link></button>
           <button><Link to="/STUDENT">STUDENT</Link></button>
@@ -53,10 +102,4 @@ class Content extends React.Component{
 
         </Switch>
       </div>
-    </Router>
-
-
-    )
-  }
-}
-
+    </Router> */}
--- react-client/src/components/signup.jsx
+++ react-client/src/components/signup.jsx
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import TeacherForm from './formData.jsx';
 import {
@@ -16,50 +15,80 @@ class SignUpForm extends Component {
             name: '',
             password: ''
         };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleChange(e) {
-        let target = e.target;
-        let name = target.name;
-        this.setState({
-          [name]: value
-        });
-    }
-    handleSubmit(e) {
-        e.preventDefault();
-        console.log('The form was submitted with the following data:');
-        console.log(this.state);
     }
     render() {
         return (
         <div className="FormCenter">
-            <form onSubmit={this.handleSubmit} className="FormFields">
-              <div className="Form1">
-                <label className="Label1" htmlFor="Email">Email</label>
-                <input type="text" id="Email" className="Input1" placeholder="Enter your Email" Email="Email" value={this.state.Email} onChange={this.handleChange} />
+            <form className="FormFields" method="POST"  action='/signup'>
+              <div >
+                <label>Email</label>
+                <input type="text"  placeholder="Enter your Email" name='email' />
               </div>
-              <div className="Form1">
-                <label className="Label1" htmlFor="password">Password</label>
-                <input type="password" id="password" className="Input1" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange} />
+              <div >
+                <label >Password</label>
+                <input type="password"  placeholder="Enter your password" name="password" />
               </div>
-              <Router>
-                <div>
-              <div className="Form1">
-              <button className="Button1" type='submit' value='login'> <Link to="/SignUpform">SignUp</Link></button>
+              
+              <div >
+              <button className="Button1" type='submit' value='sign up'> <Link to="/SignUpform">SignUp</Link></button>
               </div>
-              <Switch>
-          <Route path="/SignUpform">
-            <TeacherForm/>
-          </Route>
 
-        </Switch>
-      </div>
-    </Router>
+
 </form>
           </div>
-
         );
     }
 }
 export default SignUpForm;
+// import React, { Component } from 'react';
+// import TeacherForm from './formData.jsx';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link,
+//   useRouteMatch,
+//   useParams
+// } from "react-router-dom";
+// class SignUpForm extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             name: '',
+//             password: ''
+//         };
+
+//     }
+
+//     render() {
+//         return (
+
+// <div >
+//                 <label>Email</label>
+//                 <input type="text"  placeholder="Enter your Email" name='email' />
+//               </div>
+//               <div >
+//                 <label >Password</label>
+//                 <input type="password"  placeholder="Enter your password" name="password" />
+//               </div>
+
+//               <Router>
+//                 <div>
+
+//               <button className="Button1" type='submit'> <Link to="/SignUpform">SignUp</Link></button>
+
+//               <Switch>
+//           <Route path="/SignUpform">
+//             <TeacherForm/>
+//           </Route>
+
+//         </Switch>
+//       </div>
+//     </Router>
+
+
+
+//         );
+//     }
+// }
+// export default SignUpForm;
